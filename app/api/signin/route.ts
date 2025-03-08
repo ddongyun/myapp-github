1f9ec0cd3634747eac3c8fcc76091ca5d31995b0
ddongyun <ldy030812@gmail.com>
2025-03-08 22:30:35 +0900

    Update route.ts

--- app/api/signin/route.ts
+++ app/api/signin/route.ts
@@ -2,7 +2,7 @@ import { NextRequest, NextResponse } from "next/server";
 import { refreshNextResponseCookiesWithToken } from 'next-firebase-auth-edge/lib/next/cookies';
 
 const commonOptions = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  apiKey: process.env.FIREBASE_API_KEY,,
   cookieName: 'AuthToken',
   cookieSignatureKeys: ['Key-Should-Be-at-least-32-bytes-in-length'],
   cookieSerializeOptions: {
@@ -13,7 +13,7 @@ const commonOptions = {
     maxAge: 12 * 60 * 60 * 24 // twelve days
   },
   serviceAccount: {
-    projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+    projectId: process.env.FIREBASE_PROJECT_ID,
     clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
     privateKey: process.env.FIREBASE_PRIVATE_KEY
   }
